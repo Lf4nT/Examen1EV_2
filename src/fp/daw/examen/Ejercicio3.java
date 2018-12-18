a3f736c13645d4066e2b5acaecb818cf39483d84
Javier Moran <javiermoran7@gmail.com>
2018-12-18 09:20:05 +0100

    Donete

--- src/fp/daw/examen/Ejercicio3.java
+++ src/fp/daw/examen/Ejercicio3.java
@@ -5,33 +5,36 @@ public class Ejercicio3 {
 	/*
 	 * 2 puntos
 	 * 
-	 *  Define, después de este comentario, un método llamado 'stringToArray2D' que
-	 *  reciba como parámetro una cadena de caracteres y retorne un array de caracteres
-	 *  de dos dimensiones que tendrá exactamente dos filas y el contenido siguiente en
-	 *  cada una de ellas:
-	 *  
-	 *  	* Primera fila: caracteres de la cadena que ocupan posiciones pares (0, 2, 4, ...).
-	 *  	* Segunda fila: caracteres de la cadena que ocupan posiciones impares (1, 3, 5, ...).
-	 *  
-	 *  El número de columnas en cada fila no excederá del estrictamente necesario para
-	 *  almacenar los caracteres que se indican en cada caso.
-	 *  
+	 * Define, después de este comentario, un método llamado 'stringToArray2D' que
+	 * reciba como parámetro una cadena de caracteres y retorne un array de
+	 * caracteres de dos dimensiones que tendrá exactamente dos filas y el contenido
+	 * siguiente en cada una de ellas:
+	 * 
+	 * * Primera fila: caracteres de la cadena que ocupan posiciones pares (0, 2, 4,
+	 * ...). * Segunda fila: caracteres de la cadena que ocupan posiciones impares
+	 * (1, 3, 5, ...).
+	 * 
+	 * El número de columnas en cada fila no excederá del estrictamente necesario
+	 * para almacenar los caracteres que se indican en cada caso.
+	 * 
 	 */
-	
-	public static ...
-	
+
+	public static void arr(int[] vector) {
+
+	}
+
 	/*
 	 * 1 punto
 	 * 
-	 * Escribe en el método main las sentencias necesarias para poner a prueba
-	 * el método 'stringToArray2D' mostrando por pantalla el resultado de su ejecución.
-	 * Se mostrará en cada fila del array en una línea de la pantalla utilizando para
-	 * ello el método definido en el ejercicio 1.
+	 * Escribe en el método main las sentencias necesarias para poner a prueba el
+	 * método 'stringToArray2D' mostrando por pantalla el resultado de su ejecución.
+	 * Se mostrará en cada fila del array en una línea de la pantalla utilizando
+	 * para ello el método definido en el ejercicio 1.
 	 * 
 	 */
-	
+
 	public static void main(String[] args) {
-		
+
 	}
 
 }

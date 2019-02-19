e315809d67d4516f7a127f515891ee1cd0197281
Javier Moran <javiermoran7@gmail.com>
2019-02-19 08:56:42 +0100

    exec practica

--- Examen Programacion 20181213/src/fp/daw/examen/Ejercicio1.java
+++ Examen Programacion 20181213/src/fp/daw/examen/Ejercicio1.java
@@ -2,18 +2,6 @@ package fp.daw.examen;
 
 public class Ejercicio1 {
 
-	/*
-	 * 1 punto
-	 * 
-	 * Define, después de este comentario, un método llamado 'mostrarVector' que
-	 * reciba mediante un parámetro formal un vector de números enteros y lo muestre
-	 * por pantalla en una única línea con el formato siguiente:
-	 * 
-	 * [num1, num2, num3, ...., numN]
-	 * 
-	 * donde num1, num2, num3, ..., numN son los números almacenados en el vector.
-	 */
-
 	public static void mostrarVector(int[] vector) {
 
 		System.out.print("[");
@@ -25,18 +13,6 @@ public class Ejercicio1 {
 		System.out.print("]");
 	}
 
-	/*
-	 * 1 punto
-	 * 
-	 * Define, después de este comentario, la vesión sobrecargada del método
-	 * anterior que muestre por pantalla un vector de caracteres con el formato
-	 * siguiente:
-	 * 
-	 * ['c1', 'c2', 'c3', ...., 'cN']
-	 * 
-	 * donde 'c1', 'c2', 'c3', ...., 'cN' son los caracteres almacenados en el
-	 * vector.
-	 */
 	public static void mostrarVector(char[] vector) {
 
 		System.out.print("[");
